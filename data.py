0cc8b145cf25a47bced948df78e9091ecfb1f991
OctaNet2 <148265182+OctaNet2@users.noreply.github.com>
2024-01-09 00:15:47 -0800

    Update data.py

--- data.py
+++ data.py
@@ -4,19 +4,36 @@ import streamlit as st
 
 def main():
     st.title("My Streamlit Website")
-    st.write("Welcome to my website!")
 
-    # Add more content
-    st.header("Some Content Section")
-    st.write("This is some text.")
-    
-    # Example of a button
-    if st.button("Click me!"):
-        st.success("Button clicked!")
+    # Navbar
+    st.markdown(
+        """
+        <style>
+            .navbar {
+                display: flex;
+                padding: 10px;
+                background-color: #f1f1f1;
+            }
+            .nav-item {
+                margin-right: 20px;
+            }
+        </style>
+        """
+    , unsafe_allow_html=True)
 
-    # Example of a selectbox
-    option = st.selectbox("Choose an option", ["Option 1", "Option 2", "Option 3"])
-    st.write("You selected:", option)
+    nav_options = ["Home", "Page 1", "Page 2", "About"]
+
+    selected_nav = st.sidebar.radio("Navigation", nav_options)
+
+    # Content based on selected navbar option
+    if selected_nav == "Home":
+        st.write("Welcome to the Home Page!")
+    elif selected_nav == "Page 1":
+        st.write("This is Page 1.")
+    elif selected_nav == "Page 2":
+        st.write("This is Page 2.")
+    elif selected_nav == "About":
+        st.write("This is the About Page.")
 
     # Add more components as needed
 
